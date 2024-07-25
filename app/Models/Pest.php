b110a0c6724bc4f0ff40c894196755f77e5037e2
HamdanAs <21hamdanas@gmail.com>
2024-07-25 15:27:58 +0700

    update: decode gemini response

--- app/Models/Pest.php
+++ app/Models/Pest.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -12,6 +13,8 @@ class Pest extends Model
 
     protected $guarded = [];
 
+    public $appends = ['gemini_response_decoded'];
+
     public function garden(): BelongsTo
     {
         return $this->belongsTo(Garden::class);
@@ -20,5 +23,12 @@ class Pest extends Model
     public function commodity(): BelongsTo
     {
         return $this->belongsTo(Commodity::class);
+    }
+
+    public function geminiResponseDecoded(): Attribute
+    {
+        return Attribute::get(function ($values, $attributes) {
+            return json_decode($this->gemini_response);
+        });
     }
 }
