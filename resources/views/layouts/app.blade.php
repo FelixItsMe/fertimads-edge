fa1fd9724ea32c2193bcfa770df7b5acee1dccb8
FelixItsMe <filok5267@gmail.com>
2024-07-31 08:49:59 +0700

    add famicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,9 @@
 
   <title>{{ config('app.name', 'Laravel') }}</title>
 
+  {{-- Famicon --}}
+  <link rel="icon" href="{{ asset('images/default/famicon.svg') }}">
+
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
