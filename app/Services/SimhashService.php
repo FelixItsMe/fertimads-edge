006c95e3ce160781b989be501a98d21b9f259237
HamdanAs <21hamdanas@gmail.com>
2024-08-12 16:42:01 +0700

    fix: Update selected disease

--- app/Services/SimhashService.php
+++ app/Services/SimhashService.php
@@ -13,7 +13,7 @@ class SimhashService
     protected $comparator;
     protected $threshold;
 
-    public function __construct($threshold = 1.1)
+    public function __construct($threshold = 0.00000005)
     {
         $this->simhash = new SimHash();
         $this->threshold = $threshold;
@@ -31,9 +31,12 @@ class SimhashService
         $hash1 = $this->calculateSimhash($string1);
         $hash2 = $this->calculateSimhash($string2);
 
-        $distance = $this->comparator->compare($hash1, $hash2);
 
-        return $distance <= $this->threshold;
+        $distance = $this->comparator->compare($hash1, $hash2);
+        $float1_str = sprintf('%.8f', $distance);
+        $float2_str = sprintf('%.8f', $this->threshold);
+
+        return bccomp($float1_str, $float2_str, 14) === 1;
     }
 
     public function getDistance($string1, $string2)
